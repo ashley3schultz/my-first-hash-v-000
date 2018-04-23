32d16eeaa9fa5c0d7c097518288eaa059c4b6120
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-23 19:38:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,8 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest[muskets] = 2
-
+  shipping_manifest[] = 2
+  shipping_manifest[] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
